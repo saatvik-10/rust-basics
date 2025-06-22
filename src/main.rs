f98ba6d47403d18dc23bbba776787cc564e4990e
saatvik-10 <saatvikmadan529@gmail.com>
2025-06-23 00:12:29 +0530

    conditionals and loops

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,42 @@ fn main() {
 
     println!("greeting: {}", greeting);
 
+    let char1 = greeting.chars().nth(0);
+    
+    //not a cleaner way to handle Option
+    println!("char: {}", char1.unwrap());
+
+    //CONDITIONALS AND LOOPS
+    
     for i in 0..10 {
         println!("i: {}", i);
     }
 
-    let char1 = greeting.chars().nth(0);
+    let is_even = is_even(x);
 
-    //not a cleaner way to handle Option
-    println!("char: {}", char1.unwrap());
+    if is_even {
+        println!("{} is even", x);
+    } else {
+        println!("{} is odd", x);
+    }
+
+    let sentence = String::from("hello world");
+    let first_word = get_first_word(sentence);
+
+    println!("First word: {}", first_word);
+}
+
+fn is_even(x: i32) -> bool {
+    return x % 2 == 0;
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut ans = String::from("");
+    for char in sentence.chars() {
+        ans.push_str(char.to_string().as_str());
+        if char == ' ' {
+            break;
+        }
+    } 
+    return ans;
 }
